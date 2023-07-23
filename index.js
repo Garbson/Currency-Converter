8c7d084df8cae48682d6986aa8d1ab15471f33de
*Garbson* <garbsonsouza2602@gmail.com>
2023-07-22 23:25:44 -0500

    criação da barra de pesquisa das moedas

--- index.js
+++ index.js
@@ -1,89 +1,93 @@
-const fromCur = document.querySelector(".from select");
-const toCur = document.querySelector(".to select");
-const getBtn = document.querySelector("form button");
+
+const fromCur = document.getElementById("fromCur");
+const toCur = document.getElementById("toCur");
+const getBtn = document.getElementById("currencyConverter");
 const exIcon = document.querySelector("form .reverse");
-const amount = document.querySelector("form input");
-const exRateTxt = document.querySelector("form .result");
+const amount = document.getElementById("amount");
+const exRateTxt = document.getElementById("exRateTxt");
+const fromSearch = document.getElementById("fromSearch");
+const toSearch = document.getElementById("toSearch");
+const fromFlag = document.getElementById("fromFlag");
+const toFlag = document.getElementById("toFlag");
 
-// Event listener for currency dropdowns (select)
-
-[fromCur, toCur].forEach((select, i) => {
-    for (let curCode in Country_List) {
-        const selected = (i === 0 && curCode === "USD") || (i === 1 && curCode === "GBP") ? "selected" : "";
-        select.insertAdjacentHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option>`);
-    }
-    select.addEventListener("change", () => {
-        const code = select.value;
-        const imgTag = select.parentElement.querySelector("img");
-        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
-    });
-});
-
-// Function to get exchange rate from api
-
-async function getExchangeRate() {
-    const amountVal = amount.value || 1;
-    exRateTxt.innerText = "Getting exchange rate...";
-    try {
-        const response = await fetch(`https://v6.exchangerate-api.com/v6/5d7daba3525a80e11e344073/latest/${fromCur.value}`);
-        const result = await response.json();
-        const exchangeRate = result.conversion_rates[toCur.value];
-        const totalExRate = (amountVal * exchangeRate).toFixed(2);
-        exRateTxt.innerText = `${amountVal} ${fromCur.value} = ${totalExRate} ${toCur.value}`;
-    } catch (error) {
-        exRateTxt.innerText = "Something went wrong...";
-    }
+// Function to populate currency options in the dropdowns
+function populateCurrencyOptions(selectElement) {
+  for (let curCode in Country_List) {
+    selectElement.insertAdjacentHTML("beforeend", `<option value="${curCode}">${curCode}</option>`);
+  }
 }
 
-// Event listeners for button and exchange icon click
+// Event listener for currency dropdowns (select)
+populateCurrencyOptions(fromCur);
+populateCurrencyOptions(toCur);
 
-window.addEventListener("load", getExchangeRate);
-getBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    getExchangeRate();
+[fromCur, toCur].forEach((select) => {
+  select.addEventListener("change", () => {
+    const code = select.value;
+    const imgTag = select.nextElementSibling;
+    imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
+  });
 });
 
-exIcon.addEventListener("click", () => {
-    [fromCur.value, toCur.value] = [toCur.value, fromCur.value];
-    updateFlagImagesAndExchangeRate();
+// Function to get exchange rate from API
+async function getExchangeRate() {
+  const amountVal = amount.value || 1;
+  exRateTxt.innerText = "Getting exchange rate...";
+  try {
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/5d7daba3525a80e11e344073/latest/${fromCur.value}`);
+    const result = await response.json();
+    const exchangeRate = result.conversion_rates[toCur.value];
+    const totalExRate = (amountVal * exchangeRate).toFixed(2);
+    exRateTxt.innerText = `${amountVal} ${fromCur.value} = ${totalExRate} ${toCur.value}`;
+  } catch (error) {
+    exRateTxt.innerText = "Something went wrong...";
+  }
+}
+
+// Event listener for form submission
+getBtn.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getExchangeRate();
 });
+
+// Function to swap 'from' and 'to' currencies
+function exchangeCurrencies() {
+  const temp = fromCur.value;
+  fromCur.value = toCur.value;
+  toCur.value = temp;
+  updateFlagImagesAndExchangeRate();
+}
 
 // Function to update flag images and get the exchange rate
 function updateFlagImagesAndExchangeRate() {
-    [fromCur, toCur].forEach((select) => {
-        const code = select.value;
-        const imgTag = select.parentElement.querySelector("img");
-        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
-    });
-    getExchangeRate();
+  [fromCur, toCur].forEach((select) => {
+    const code = select.value;
+    const imgTag = select.nextElementSibling;
+    imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
+  });
+  getExchangeRate();
 }
 
 // Function to filter currencies based on input letter
-function filterCurrencies() {
-    const letter = currencyInput.value.trim().toUpperCase();
+function filterCurrencies(currencyType) {
+  const letter = (currencyType === "from" ? fromSearch.value : toSearch.value).trim().toUpperCase();
+  const filteredOptions = Object.keys(Country_List).filter(curCode => curCode.includes(letter));
 
-    // Filter options based on the input letter
-    const filteredOptionsFrom = Object.keys(Country_List).filter(curCode => curCode.includes(letter));
-    const filteredOptionsTo = Object.keys(Country_List).filter(curCode => curCode.includes(letter));
+  const selectElement = (currencyType === "from" ? fromCur : toCur);
+  const selectedValue = selectElement.value;
 
-    // Clear the dropdown options
-    fromCur.innerHTML = "";
-    toCur.innerHTML = "";
+  // Clear the dropdown options
+  selectElement.innerHTML = "";
 
-    // Re-populate the dropdown options with the filtered currencies
-    filteredOptionsFrom.forEach(curCode => {
-        const selected = curCode === fromCur.value ? "selected" : "";
-        fromCur.insertAdjacentHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option>`);
-    });
+  // Re-populate the dropdown options with the filtered currencies
+  filteredOptions.forEach(curCode => {
+    const selected = curCode === selectedValue ? "selected" : "";
+    selectElement.insertAdjacentHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option>`);
+  });
 
-    filteredOptionsTo.forEach(curCode => {
-        const selected = curCode === toCur.value ? "selected" : "";
-        toCur.insertAdjacentHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option>`);
-    });
-
-    // Update the flag images and get the new exchange rate
-    updateFlagImagesAndExchangeRate();
+  // Update the flag images and get the new exchange rate
+  updateFlagImagesAndExchangeRate();
 }
 
-const currencyInput = document.querySelector("form input");
-currencyInput.addEventListener("input", filterCurrencies);
+// Initial update of flag images and exchange rate
+updateFlagImagesAndExchangeRate();
